120e661b2a3a28f8903ab5976fca447cdc3ba04e
TC94615 <tclee0117@gmail.com>
2016-04-27 18:18:31 +0800

    Add tests.

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -9,6 +9,8 @@
 // https://github.com/kiwi-bdd/Kiwi
 
 #import <OSRESTfulClient/OSRESTfulEndpoint.h>
+#import <OSRESTfulClient/OSRequestBuilder.h>
+#import <AFNetworking/AFURLSessionManager.h>
 
 SPEC_BEGIN(InitialTests)
 
@@ -22,6 +24,28 @@ SPEC_BEGIN(InitialTests)
                 OSRESTfulEndpoint *endpoint = [[OSRESTfulEndpoint alloc] initWithBaseURLString:@"https://api.github.com"];
                 [endpoint updateBaseURLString:@"https://api.github.com.tw"];
                 [[endpoint.baseURLString should] equal:@"https://api.github.com.tw"];
+            });
+        });
+    });
+
+    describe(@"Builder tests", ^{
+        context(@"Set path tests", ^{
+            it(@"No parameter", ^{
+                OSRESTfulEndpoint *endpoint = [[OSRESTfulEndpoint alloc] initWithBaseURLString:@"http://localhost:3000"];
+                AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+                OSRequestBuilder *builder = [[OSRequestBuilder alloc] initWithBaseURLString:endpoint.baseURLString
+                                                                             sessionManager:manager];
+                builder.setPath(@"/posts/1");
+            });
+            it(@"One parameter", ^{
+                OSRESTfulEndpoint *endpoint = [[OSRESTfulEndpoint alloc] initWithBaseURLString:@"http://localhost:3000"];
+                AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
+                OSRequestBuilder *builder = [[OSRequestBuilder alloc] initWithBaseURLString:endpoint.baseURLString
+                                                                             sessionManager:manager];
+                builder.setPath(@"/posts/{id}", @{@"id" : @"1"});
+            });
+            it(@"Two parameters", ^{
+
             });
         });
     });
